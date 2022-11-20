47b16cc766e3bb5e93ca1b8a372255bcf3e3b711
ElliotHiester <elliotawesome05@gmail.com>
2022-11-20 12:52:25 -0500

    Elliot H 11/20

--- scripts/nav-bar.js
+++ scripts/nav-bar.js
@@ -1,20 +1,49 @@
 console.log("nav-bar.js loaded");
 
+function barExpand() {
+    $()
+}
+
+
+//initial text size
+$(".nav-text").css("font-size", "" + window.innerHeight/38 + "px");
+
+//initial bar height
+$(".select-bar").css("height", "" + window.innerHeight/258.5 + "px");
 
 //hover text color change
 let navItemList = $(".nav-item");
 
 navItemList.hover(function() {
-    $(this).children().css("color", "#e74c3c");//red text when hovered
+    $(this).children(".nav-text").css("color", "#e74c3c");//red text when hovered
+
+    $(this).children(".select-bar").animate({ //animate selection bar
+        width: "100px"
+    }, "slow");
+
 }, function() {
-    $(this).children().css("color", "#848484"); //grey text when not hovered
+    $(this).children(".nav-text").css("color", "#848484"); //grey text when not hovered
+
+    $(this).children(".select-bar").animate({
+        width: "4px"
+        }, "slow");//select bar returns when not hovered
 });
 
-//text scales with window
+//text and navbar scales with window
 console.log(window.innerHeight);
 
 window.onresize = function() {
-    $(".nav-text").css("text-size", "400px");
+    $(".nav-text").css({
+        "font-size": "" + window.innerHeight/38 + "px", 
+        "top": "25%"
+    });
+
+    $(".nav-item").css({
+        "transform-origin": "center center"
+    });
+
+    
+    $(".select-bar").css("height", "" + window.innerHeight/258.5 + "px");
 };
 
 
