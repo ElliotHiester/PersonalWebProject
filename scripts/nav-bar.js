8157ad4b5429f97471beee351286af3fcc86b400
ElliotHiester <elliotawesome05@gmail.com>
2022-11-22 22:29:31 -0500

    Elliot H 11/22

--- scripts/nav-bar.js
+++ scripts/nav-bar.js
@@ -1,53 +1,41 @@
 console.log("nav-bar.js loaded");
 
+//vars
+let navItemList = $(".nav-item");
+let selectBarWidth = "--select-bar-factor";
+
+//initial bar width
+document.documentElement.style.setProperty(selectBarWidth, window.innerWidth/35);
+
 //initial text size
 $(".nav-text").css("font-size", "" + window.innerHeight/38 + "px");
 
-//initial bar height and width
-$(".select-bar").css("height", "" + window.innerHeight/258.5 + "px");
-$(".select-bar").css("width", "3px");
-
-
+//initial select bar size
+$(".select-bar").css("height", "" + window.innerHeight/400.5 + "px");
 
 //hover text color change
-let navItemList = $(".nav-item");
-
 navItemList.hover(function() {
     $(this).children(".nav-text").css("color", "#e74c3c");//red text when hovered
-
-    /*$(this).children(".select-bar").animate({ //animate selection bar
-        width: "30%",
-        //right: "30%"
-    }, "slow");*/
-
-    $(this).children(".select-bar").animate({
-        transform: scaleX(5)
-    }, "slow");
-
 }, function() {
     $(this).children(".nav-text").css("color", "#848484"); //grey text when not hovered
-
-    $(this).children(".select-bar").animate({
-        width: "0px"
-    }, "slow");//select bar returns when not hovered
 });
 
 //text and navbar scales with window
 console.log(window.innerHeight);
+console.log(window.innerWidth);
 
 window.onresize = function() {
+    //text resize with window
     $(".nav-text").css({
         "font-size": "" + window.innerHeight/38 + "px", 
         "top": "25%"
     });
     
+    //select bar height scales with window
+    $(".select-bar").css("height", "" + window.innerHeight/400.5 + "px");
 
-    $(".nav-item").css({
-        "transform-origin": "center center"
-    });
-
-    
-    $(".select-bar").css("height", "" + window.innerHeight/258.5 + "px");
+    //select bar scales with window width
+    document.documentElement.style.setProperty(selectBarWidth, window.innerWidth/35);
 };
 
 
